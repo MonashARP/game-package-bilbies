5c759200e56b1942793015a238e45b6ee1738a6b
rong xu <rxuu0059@student.monash.edu>
2025-05-17 22:00:56 +1000

    ran unit test of deal_hand function and it was appreved

--- tests/testthat/test-deal_hand.R
+++ tests/testthat/test-deal_hand.R
@@ -1,11 +1,19 @@
-test_that("deal_hand returns correct number of cards", {
-  hand <- deal_hand(2)
-  expect_type(hand, "character")
-  expect_length(hand, 2)
-})
+test_that("deal_hand returns correct number of cards and updates deck", {
+  deck <- create_shuffled_deck()
+  result <- deal_hand(deck, 2)
 
-test_that("deal_hand draws unique cards", {
-  hand <- deal_hand(10)
-  expect_equal(length(unique(hand)), 10)
-})
+  expect_type(result, "list")
+  expect_named(result, c("hand", "deck"))
+  expect_length(result$hand, 2)
+  expect_length(result$deck, length(deck) - 2)
 
+  # Check that counts of cards have decreased properly
+  orig_counts <- table(deck)
+  hand_counts <- table(result$hand)
+  new_counts <- table(result$deck)
+
+  # For all cards in hand, check that updated deck has correct reduced counts
+  for (card in names(hand_counts)) {
+    expect_equal(orig_counts[card], hand_counts[card] + new_counts[card])
+  }
+})
