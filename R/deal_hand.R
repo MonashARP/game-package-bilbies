acc8348d48fbae9cbe4b55c3469f9b9b007f1b76
rong xu <rxuu0059@student.monash.edu>
2025-05-17 17:27:02 +1000

    refine deal_hand function

--- R/deal_hand.R
+++ R/deal_hand.R
@@ -1,11 +1,19 @@
 #' Deal a Blackjack Hand
 #'
-#' Randomly draw cards from the deck
+#' Deals cards from a given deck and removes them from the deck.
 #'
+#' @param deck A character vector representing the current deck (e.g., output of `create_shuffled_deck()`)
 #' @param n Number of cards to deal (default = 2)
-#' @return A character vector like "10♥", "A♠"
+#'
+#' @return A list with:
+#' \describe{
+#'   \item{hand}{Character vector of dealt cards (length = n)}
+#'   \item{deck}{The updated deck after removing dealt cards}
+#' }
 #' @export
-deal_hand <- function(n = 2) {
-  cards <- paste0(deck$rank, deck$suit)
-  sample(cards, n)
+deal_hand <- function(deck, n = 2) {
+  if (length(deck) < n) stop("Not enough cards left in the deck to deal.")
+  hand <- deck[1:n]
+  remaining_deck <- deck[-(1:n)]
+  return(list(hand = hand, deck = remaining_deck))
 }
