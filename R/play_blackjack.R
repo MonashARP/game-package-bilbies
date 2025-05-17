1491747ef3f8a997eaa5445ffdb4bf9aa32e5e92
rong xu <rxuu0059@student.monash.edu>
2025-05-18 00:14:27 +1000

    remane function name

--- R/play_blackjack.R
+++ R/play_blackjack.R
@@ -1,14 +1,36 @@
-#' Simulate a Blackjack Round
+#' Play One Round of Blackjack with Action
 #'
-#' Deals hands to player and dealer, scores them, and reports outcome
+#' This function simulates a full round of Blackjack, allowing the player to hit or stand.
+#'
+#' @param n_players Number of players (default is 1)
 #'
 #' @return A list with hands, scores, and outcome
+#'
 #' @export
-play_blackjack <- function() {
-  player_hand <- deal_hand(2)
-  dealer_hand <- deal_hand(2)
-  player_score <- card_value(player_hand)
-  dealer_score <- card_value(dealer_hand)
+play_blackjack_action <- function(n_players = 1) {
+  # Deal two cards to the player
+  str <- deal_hand(deck, 2)
+  player_hand <- str$hand
+  deck        <- str$deck
+  # Deal two cards to the dealer
+  str <- deal_hand(deck, 2)
+  dealer_hand <- str$hand
+  deck        <- str$deck
+
+  cat("Dealer shows:", dealer_hand[1], "?\n")
+
+  # Create a shuffled deck of cards
+  deck <- create_shuffled_deck()
+  # Player's turn
+  str   <- player_turn(player_hand, deck)
+  player_hand  <- str$hand
+  deck         <- str$deck
+  player_score <- str$total
+  # Dealer's turn
+  str <- dealer_turn(dealer_hand, deck)
+  dealer_hand  <- str$hand
+  deck         <- str$deck
+  dealer_score <- str$total
 
   result <- if (player_score > 21) {
     "Player busts"
