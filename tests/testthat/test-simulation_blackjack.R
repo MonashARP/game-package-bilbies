9058808c7c1cb04114e36b2bc0b003728cc394a9
Jakey-vn <dtra0044@student.monash.edu>
2025-05-19 23:43:44 +1000

    add tempotary unit tests for simulation game

--- tests/testthat/test-simulation_blackjack.R
+++ tests/testthat/test-simulation_blackjack.R
@@ -1,8 +1,4 @@
 test_that("simulation_blackjack runs and returns a table", {
-  with_mocked_bindings(
-    result <- simulation_blackjack(n_sim = 3),
-    readline = function(prompt) "13"
-  )
-
+  result <- simulation_blackjack()
   expect_s3_class(result, "table")
 })
