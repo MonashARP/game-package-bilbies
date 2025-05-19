179dbdb10ef835131a72ba4f7e47b6de0c00dd14
Jakey-vn <dtra0044@student.monash.edu>
2025-05-19 23:27:41 +1000

    update unit test for auto_player_turn

--- tests/testthat/test-auto_player_turn.R
+++ tests/testthat/test-auto_player_turn.R
@@ -1,11 +1,29 @@
-test_that("auto_player_turn works as expected", {
-  deck <- c("2♠", "3♦", "4♣")
-  player_hand <- c("2♣")
-  threshold <- 10
+test_that("auto_player_turn draws no cards when total ≥ threshold", {
+  deck <- rep("2♠", 208)
+  # Starting hand total = 16
+  starting_hand <- c("8♠", "8♦")
 
-  result <- auto_player_turn(player_hand, deck, threshold)
+  res <- auto_player_turn(starting_hand, deck, threshold = 16)
 
-  expect_gte(result$total, threshold)
-  expect_gt(length(result$hand), 1)
-  expect_lt(length(result$deck), length(deck))
+  # No draws should occur
+  expect_equal(res$total, 16L)
+  expect_equal(res$hand, starting_hand)
+  expect_equal(res$deck, deck)
+})
+
+test_that("auto_player_turn draws until threshold reached", {
+  deck <- rep("2♠", 208)
+  # Starting hand total = 10
+  starting_hand <- c("5♠", "5♦")
+
+  str <- auto_player_turn(starting_hand, deck, threshold = 16)
+
+  # Draws of 2 until total ≥ 16. That become 3 draws: 10 + 2 + 2 + 2 = 16
+  expect_equal(str$total, 16L)
+  expect_length(str$hand, 2 + 3)
+  expect_equal(
+    str$hand,
+    c(starting_hand, rep("2♠", 3))
+  )
+  expect_length(str$deck, length(deck) - 3)
 })
