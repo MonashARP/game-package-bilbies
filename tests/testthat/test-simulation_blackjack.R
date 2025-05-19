c46455a4adcfaa50daebe40b9af7830b17b145b3
Jakey-vn <dtra0044@student.monash.edu>
2025-05-20 09:36:05 +1000

    modify unit test to work for simulation

--- tests/testthat/test-simulation_blackjack.R
+++ tests/testthat/test-simulation_blackjack.R
@@ -1,4 +1,4 @@
-test_that("simulation_blackjack runs and returns a table", {
-  result <- simulation_blackjack()
+test_that("simulation_blackjack run with threshold of 16 and returns a table", {
+  result <- simulation_blackjack(16)
   expect_s3_class(result, "table")
 })
