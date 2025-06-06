f60267045cb2e65ed45e0f947bc32ffb027655b4
Yiran Yao <yyao0066@student.monash.edu>
2025-06-06 14:15:08 +1000

    Needed for pkgdown

---
+++ R/blackjack-package.R
@@ -0,0 +1,9 @@
+#' @title Blackjack Package
+#' @name blackjack-package
+#' @description
+#' The `blackjack` package provides tools to simulate the game of Blackjack in R.
+#' It supports single and multiplayer modes, dealer logic, and strategy testing.
+#'
+#' @useDynLib blackjack, .registration = TRUE
+#' @importFrom Rcpp evalCpp
+NULL
